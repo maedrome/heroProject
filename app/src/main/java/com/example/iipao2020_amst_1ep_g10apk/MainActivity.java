c2a49325b6106a8b538b29a7d65bfae16bd6c15e
maedrome <maedrome@espol.edu.ec>
2021-07-09 17:27:01 -0500

    inicial

---
+++ app/src/main/java/com/example/iipao2020_amst_1ep_g10apk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.iipao2020_amst_1ep_g10apk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
