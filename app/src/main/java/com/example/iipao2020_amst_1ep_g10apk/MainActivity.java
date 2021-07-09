1327ba16ddf0145652c918d861fe70c4d6b6589c
Daniel Robles <drobles@espol.edu.ec>
2021-07-09 17:37:20 -0500

    intent boton

--- app/src/main/java/com/example/iipao2020_amst_1ep_g10apk/MainActivity.java
+++ app/src/main/java/com/example/iipao2020_amst_1ep_g10apk/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.iipao2020_amst_1ep_g10apk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.BtnSearch);
+
+        button.setOnClickListener((view)-> {
+
+                    startActivity(new Intent(MainActivity.this, Resultado.class));
+                }
+
+        );
+
     }
+
 }
